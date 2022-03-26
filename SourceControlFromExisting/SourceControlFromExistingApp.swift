9cc0fb42ca691b1b5e0cc970981728589aabc1d4
Alik Myroniv <almyro@icloud.com>
2022-03-26 13:05:48 -0700

    Initial Commit

---
+++ SourceControlFromExisting/SourceControlFromExistingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlFromExistingApp.swift
+//  SourceControlFromExisting
+//
+//  Created by Alik Myroniv on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlFromExistingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
